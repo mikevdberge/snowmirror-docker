37678e32c3b203c5f02797834988f98b350d1244
Mike van den Berge <mikevdberge@users.noreply.github.com>
2024-05-28 16:34:22 +0200

    Update run.sh with otel options

--- run.sh
+++ run.sh
@@ -76,6 +76,9 @@ fi
 # Export java properties
 echo "Memory settings: Xms=$APP_XMS, Xmx=$APP_XMX"
 
+export JAVA_TOOL_OPTIONS="-javaagent:$_curr/libs/opentelemetry-javaagent.jar"
+export OTEL_SERVICE_NAME="SnowMirror"
+
 export JAVA_OPTS="-Duser.timezone=UTC -Duser.country=en -Duser.language=en -DsnowMirror.logDir=$_curr/logs -DsnowMirror.dataDir=file://$_curr/snow-mirror/data -DsnowMirror.properties.location=file://$_curr/snow-mirror/conf/ -DsnowMirror.config.db.location=file://$_curr/snowMirror.properties -DsnowMirror.config.location=file://$_curr/snow-mirror/conf/ -DsnowMirror.themesDir="file:///$_curr/snow-mirror-themes/" -Dspring.profiles.active=live -Dtomcat.http.port=$APP_PORT -Dtomcat.http.controlport=$CONTROL_PORT -Dtomcat.snowMirror.context=$APP_CONTEXT $APP_JAVA_OPTS"
 export CATALINA_OPTS="-Xms$APP_XMS -Xmx$APP_XMX $APP_CATALINA_OPTS"
 
